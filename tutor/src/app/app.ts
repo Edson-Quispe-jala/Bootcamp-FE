7418066e9164c5bbef53e5d858bde369d8de0d78
Edson <Edson.Quispe@jala.university>
2026-05-25 20:26:30 -0400

    complete module 6 start phase 3

--- tutor/src/app/app.ts
+++ tutor/src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MOCK_RECIPES } from './mock-recipes';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,6 +13,7 @@ export class App {
   protected readonly title = signal('My Recipe Box.');
   protected readonly recipe = signal(MOCK_RECIPES[0]);
   protected readonly servings = signal(0);
+  protected readonly adjustedIngredients = computed(() => this.recipe().ingredients.map(ingredient =>  ({ name: ingredient.name, quantity: ingredient.quantity * this.servings() })));
 
   protected button1Clicked() {
     console.log('Button 1 was clicked!');
