aae0c215faf82edd4e1c35cc44080cd7d11d136b
Edson <Edson.Quispe@jala.university>
2026-05-31 19:58:55 -0400

    complete module 8

--- tutor/src/app/app.ts
+++ tutor/src/app/app.ts
@@ -9,27 +9,4 @@ import { JsonPipe } from '@angular/common';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('My Recipe Box.');
-  protected readonly recipe = signal(MOCK_RECIPES[0]);
-  protected readonly servings = signal(0);
-  protected readonly adjustedIngredients = computed(() => this.recipe().ingredients.map(ingredient =>  ({ name: ingredient.name, quantity: ingredient.quantity * this.servings() })));
-
-  protected button1Clicked() {
-    console.log('Button 1 was clicked!');
-    this.recipe.set(MOCK_RECIPES[0]);
-  }
-
-  protected button2Clicked() {
-    console.log('Button 2 was clicked!');
-    this.recipe.set(MOCK_RECIPES[1]);
-  }
-
-  protected addServings() {
-    this.servings.update(val => val + 1);
-  }
-
-  protected substractServings() {
-    this.servings.update(val => val - 1);
-  }
-}
+export class App { }
---
+++ tutor/src/app/recipe-list/recipe-list.ts
@@ -0,0 +1,33 @@
+import { Component, computed, signal } from '@angular/core';
+import { MOCK_RECIPES } from '../mock-recipes';
+
+@Component({
+  selector: 'app-recipe-list',
+  imports: [],
+  templateUrl: './recipe-list.html',
+  styleUrl: './recipe-list.css',
+})
+export class RecipeList {
+  protected readonly title = signal('My Recipe Box.');
+  protected readonly recipe = signal(MOCK_RECIPES[0]);
+  protected readonly servings = signal(0);
+  protected readonly adjustedIngredients = computed(() => this.recipe().ingredients.map(ingredient =>  ({ name: ingredient.name, quantity: ingredient.quantity * this.servings() })));
+
+  protected button1Clicked() {
+    console.log('Button 1 was clicked!');
+    this.recipe.set(MOCK_RECIPES[0]);
+  }
+
+  protected button2Clicked() {
+    console.log('Button 2 was clicked!');
+    this.recipe.set(MOCK_RECIPES[1]);
+  }
+
+  protected addServings() {
+    this.servings.update(val => val + 1);
+  }
+
+  protected substractServings() {
+    this.servings.update(val => val - 1);
+  }
+}
