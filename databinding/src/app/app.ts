56f95a5621051762be9368ced487a886fba00007
Edson <Edson.Quispe@jala.university>
2026-06-11 17:12:55 -0400

    first example

---
+++ databinding/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Child } from "./child/child";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Child],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected count = 0;
+
+  incrementCount() {
+    this.count++;
+  }
+}
